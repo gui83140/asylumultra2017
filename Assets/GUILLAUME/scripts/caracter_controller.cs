11fea307d4368618b82d03bf7239090d396f7ea1
Pc-Guillaume07\Guillaume <guillaume.dufour07@gmail.com>
2017-11-04 00:19:39 +0100

    1erversionmvtplayer

---
+++ Assets/GUILLAUME/scripts/caracter_controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class caracter_controller : MonoBehaviour {
+    float posX; float posY; public float walkspeed; float runspeed;
+    // Use this for initialization
+    void Start() {
+        runspeed = 1;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetButton("run"))
+            {
+            runspeed = 1.4f;
+
+        }
+
+        posX = Input.GetAxis("Horizontal");
+        posY = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(posX, 0, posY) * walkspeed * runspeed;
+
+
+    }
+}
