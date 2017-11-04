14cf2df7538aa9dd6711e59a7b87e938b8e1c57d
Stonga <gaston.robert.auditeur@lecnam.net>
2017-11-04 17:48:47 +0100

    Empreintes
    Un bout d'empreintes

---
+++ Assets/GASTON/Scripts/apparition_footprint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class apparition_footprint : MonoBehaviour {
+
+    public GameObject empreinte;
+    public GameObject FootMoove;
+    public GameObject Socket;
+
+    bool OnDown = false;
+
+    void Update()
+    {
+
+        empreinte.SetActive(true);
+
+        if (OnDown)
+        {
+            empreinte.SetActive(false);        
+            OnDown = false;
+
+            Instantiate(FootMoove, Socket.transform.position, Socket.transform.rotation);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            OnDown = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            OnDown = true;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            OnDown = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            OnDown = true;
+        }
+    }
+
+}
