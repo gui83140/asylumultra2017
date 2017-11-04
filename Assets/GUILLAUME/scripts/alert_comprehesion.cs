d7acf73b5d5fb33a70b9a5ef65c581d0e4c06e08
Pc-Guillaume07\Guillaume <guillaume.dufour07@gmail.com>
2017-11-04 17:47:07 +0100

    bullshit(detectioneauquandmeme)

---
+++ Assets/GUILLAUME/scripts/alert_comprehesion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pas_follow : MonoBehaviour {
+        public GameObject target;
+       
+        private Vector3 offset; 
+
+	// Use this for initialization
+	void Start () {
+        offset = new Vector3(0, 1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //targetedposition = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+
+        transform.position =  target.transform.position + offset;
+    }
+}
